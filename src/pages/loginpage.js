ac6a34ea84c974c3a683555c9d4e50976ecbed1b
Sakyiama Dennis <albacruise99@gmail.com>
2024-06-11 15:28:20 +0000

    first commit

---
+++ src/pages/loginpage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import fdalogo from '../assets/fdalogo.png'
+import "./logincss.css"
+
+function Login(){
+
+    return(
+        <div className="login-container">
+        <div className="login-box">
+
+        <img src={fdalogo} alt="FDA Logo" className="logo" />
+        
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+          </div>
+           
+          <button type="submit" className="login-button">Login</button>
+        </form>
+
+        <div className="links">
+          <p> If you do not have an account <a href="">Click Here</a> </p>
+          <p>Have you forgotten your password? <a href="/reset"> Reset Now</a></p>
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Login;
