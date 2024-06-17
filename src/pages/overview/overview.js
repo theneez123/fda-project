41d0db769c4bd8c6bd4f165978d66dd85b520eec
Sakyiama Dennis <albacruise99@gmail.com>
2024-06-17 08:16:09 +0000

    a

--- src/pages/overview/overview.js
+++ src/pages/overview/overview.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import './overviewcss.css';
+import SideNav from '../../components/sidenav/SideNav.js';
+import TopNav from '../../components/topnav/TopNav.js';
+
 
 const overview = () => {
+
   return (
-    <div>
-        sdfghjk
+    <div className="container">
+      <SideNav/>
+      <div className="ContentArea">
+
+        <TopNav/>
+        <div className="MainContent">         
+          Main page
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default overview
