6d236c8a3f2fa6d83ac988d02b72430d336f02a0
Sakyiama Dennis <albacruise99@gmail.com>
2024-06-21 03:41:13 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Login from './pages/loginpage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 import Assetspage from './pages/assetspage/Assetspage';
 import Userspage from './pages/users/Userspage';
 import Overview from './pages/overview/Overview';
-import Departmentpage from './pages/departments/Departmentpage';
+import Departemntpage from './pages/departments/Departmentpage';
 import Systemlogs from './pages/systemlogs/Systemlogpage';
+
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/overview' element={<Overview />} />
-          <Route path='/assets' element={<Assetspage />} />
-          <Route path='/users' element={<Userspage />} />
-          <Route path='/departments' element={<Departmentpage />} />
-          <Route path='/systemlogs' element={<Systemlogs />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Router>
+        <div className="App">
+        
+          <Routes>
+            <Route path='/' element = {<Login/>} />
+            <Route path='/overview' element = {<Overview/>} />
+            <Route path='/assets' element = {<Assetspage/>} />
+            <Route path='/users' element = {<Userspage/>} /> 
+            <Route path='/departments' element = {<Departemntpage/>} />
+            <Route path='/systemlogs' element = {<Systemlogs/>} />
+          </Routes>     
+          
+
+        </div>
+    </Router>
   );
 }
 
 export default App;
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
