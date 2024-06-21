7460575a627b5c777c62296413a22083b4b58098
Sakyiama Dennis <albacruise99@gmail.com>
2024-06-21 03:27:34 +0000

    a

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import Login from './pages/loginpage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './pages/loginpage';
 import Assetspage from './pages/assetspage/Assetspage';
 import Userspage from './pages/users/Userspage';
 import Overview from './pages/overview/Overview';
-import Departemntpage from './pages/departments/Departmentpage';
+import Departmentpage from './pages/departments/Departmentpage';
 import Systemlogs from './pages/systemlogs/Systemlogpage';
-
 
 function App() {
   return (
-    <Router>
-        <div className="App">
-        
-          <Routes>
-            <Route exact path='/' element = {<Login/>} />
-            <Route path='/overview' element = {<Overview/>} />
-            <Route path='/assets' element = {<Assetspage/>} />
-            <Route path='/users' element = {<Userspage/>} /> 
-            <Route path='/departments' element = {<Departemntpage/>} />
-            <Route path='/systemlogs' element = {<Systemlogs/>} />
-          </Routes>     
-          
-
-        </div>
-    </Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/overview' element={<Overview />} />
+          <Route path='/assets' element={<Assetspage />} />
+          <Route path='/users' element={<Userspage />} />
+          <Route path='/departments' element={<Departmentpage />} />
+          <Route path='/systemlogs' element={<Systemlogs />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
